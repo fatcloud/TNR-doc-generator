ed87e0a7dbbc6634d9134e91d6f4a956858d2763
fatcloud <victor19871226@hotmail.com>
2017-01-15 03:47:40 +0800

    add error handling, scale image size

--- tasks/tnvrform/form_filler.py
+++ tasks/tnvrform/form_filler.py
@@ -7,100 +7,118 @@ from reportlab.lib.utils import ImageReader
 
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.colors import yellow, red
 import reportlab.lib.fonts
 
 from datetime import datetime,timedelta
 
 pdfmetrics.registerFont(TTFont('STHeiti','data/STHeiti-Medium.ttc'))
 
-#reportlab.lib.fonts.ps2tt = lambda psfn: ('STHeiti', 0, 0)
-#reportlab.lib.fonts.tt2ps = lambda fn,b,i: 'STHeiti'
+from glob import glob
+import re
 
 
-def tnvr_form_filler(row, canvas):
-    
-    '''
-    # 顯示每一筆輸入資料
-    print('Here is a single row:', row, '\n')
-    
-    # 示範貼上文字
-    # canvas.setFont()
-    print(pdfmetrics.getRegisteredFontNames())
-    canvas.setFont('STHeiti', 12)
-    canvas.setFillColorRGB(1,0,0)
-    canvas.drawString(10, 100, ' '.join(map(str, row)))
-
-    # 示範貼上圖片
-    im = ImageReader("img/cat.jpg")
-    canvas.drawImage(im, 50, 10, width=100,height=100)
-
-    # ========= 以上為示範程式碼，需刪除後才能開始正式工作
-    # TODO    
-    '''
+def tnvr_form_filler(row, canvas, row_index):
 
     def period(t):
-        time = str(row[2])
+        # convert 105/12/26 to 0105/12/26 to match YYYY/MM/DD pattern
+        time = '0' + str(row[2])
         date = datetime.strptime(time, "%Y/%m/%d")
         db = date - timedelta(days = 8)
         de = date - timedelta(days = 1)
         period = db.strftime("%Y/%m/%d")+' - '+ de.strftime("%m/%d")
         return period
     
+    
+    def value(data):
+        return data.split('/')[0]
+    
+    def reason(data):
+        return data.split('/')[-1] if '/' in row[1] else ''
+         
 
-
-    form = {
-        'Ta1' : {'type':'str' , 'source':'row[6]', 'coord':(190, 685)},
-        'Ta2' : {'type':'opt' , 'source':'row[7]', 'coord':{'公':(193,662),'母':(347,662)}},
-        'Ta3' : {'type':'opt' , 'source':'row[8]', 'coord':{'嬰':(227,637),'幼':(217,624),'成':(309,637),'老':(301,624)}},
-        'Ta4' : {'type':'opt', 'source':'row[1].split(\'/\')[0]', 'coord':{'無':(233,600),'有':(233,587)}},
-        'Ta4r' : {'type':'str', 'source':'\' \' if \'無\' in row[1] else row[1].split(\'/\')[-1]', 'coord':(257,587)},
-        'Timg' : {'type':'img', 'source':'before', 'coord':(421,576)},
-        'Tb1' : {'type':'str' , 'source':'\'某某某\'', 'coord':(203,550)}, #constant?
-        'Tb2' : {'type':'str' , 'source':'str(row[2])', 'coord':(235,533)},
-        'Tb3a' : {'type':'str' , 'source':'row[4]', 'coord':(213,510)},
-        'Tb3b' : {'type':'str' , 'source':'row[5]', 'coord':(250,510)},
-        'Tb41' : {'type':'str' , 'source':'\'公告於懷生相信動物協會FB公開版面\'', 'coord':(246,490)}, #constant?
-        'Tb42' : {'type':'str' , 'source':'period(row[2])', 'coord':(246,473)}, 
-        'NVa1' : {'type':'str' , 'source':'str(row[9])', 'coord':(235,451)},
-        'NVa2' : {'type':'opt' , 'source':'row[7]', 'coord':{'公':(216,434), '母':(280,434)}},
-        'NVa3' : {'type':'opt' , 'source':'row[13].split(\'/\')[0]', 'coord':{'否':(251, 415),'是':(222,395)}},
-        'NVa3r' : {'type':'str' , 'source':'\' \' if \'否\' in row[13] else row[13].split(\'/\')[-1]', 'coord':(305,395)},
-        'NVa4' : {'type':'opt' , 'source':'row[14].split(\'/\')[0]', 'coord':{'否':(205, 376),'是':(256,376),'安樂死':(205,357)}},
-        'NVa4r1' : {'type':'str' , 'source':'row[14].split(\'/\')[-1] if \'是\' in row[14][0] else \' \'', 'coord':(307,376)},
-        'NVa4r2' : {'type':'str' , 'source':'row[14].split(\'/\')[-1] if \'安樂死\' in row[14] else \' \'', 'coord':(285,357)},
-        'NVa5' : {'type':'opt' , 'source':'row[7]', 'coord':{'公':(216, 338),'母':(280,338)}},
-        'NVa6' : {'type':'str' , 'source':'str(row[12])', 'coord':(238, 307)},
-        'NVa7' : {'type':'opt' , 'source':'\'有\'', 'coord':{'有':(251, 280),'無':(333,280)}}, #constant option
-        'NVimg' : {'type':'img' , 'source':'after', 'coord':(421, 307)},
-        'Ra1' : {'type':'str' , 'source':'str(row[10])', 'coord':(230, 240)},
-        'Ra2' : {'type':'opt' , 'source':'\'補助\'', 'coord':{'補助':(200, 197),'自費':(257, 197)}}, #constant option
+    form_name = '台北市街犬絕育防疫 TNVR 執行計畫流程紀錄表'
+    form_items = [
+        {'item':'Ta1', 'type':'str'   , 'source':'row[6]', 'coord':(190, 685)},
+        {'item':'Ta2', 'type':'opt'   , 'source':'row[7]', 'coord':{'公':(193,662),'母':(347,662)}},
+        {'item':'Ta3', 'type':'opt'   , 'source':'row[8]', 'coord':{'嬰':(227,637),'幼':(217,624),'成':(309,637),'老':(301,624)}},
+        {'item':'Ta4', 'type':'opt'   , 'source':'value(row[1])', 'coord':{'無':(233,600),'有':(233,587)}},
+        {'item':'Ta4r', 'type':'str'  , 'source': 'reason(row[1])', 'coord':(257,587)},
+        {'item':'Timg', 'type':'img'  , 'source':'row[3]+\'pre\'', 'coord':(421,576), 'width':120},
+        {'item':'Tb1', 'type':'str'   , 'source':'\'某某某\'', 'coord':(203,550)}, #constant?
+        {'item':'Tb2', 'type':'str'   , 'source':'str(row[2])', 'coord':(235,533)},
+        {'item':'Tb3a', 'type':'str'  , 'source':'row[4]', 'coord':(213,510)},
+        {'item':'Tb3b', 'type':'str'  , 'source':'row[5]', 'coord':(250,510)},
+        {'item':'Tb41', 'type':'str'  , 'source':'\'公告於懷生相信動物協會FB公開版面\'', 'coord':(246,490)}, #constant?
+        {'item':'Tb42', 'type':'str'  , 'source':'period(row[2])', 'coord':(246,473)}, 
+        {'item':'NVa1', 'type':'str'  , 'source':'str(row[9])', 'coord':(235,451)},
+        {'item':'NVa2', 'type':'opt'  , 'source':'row[7]', 'coord':{'公':(216,434), '母':(280,434)}},
+        {'item':'NVa3', 'type':'opt'  , 'source':'value(row[13])', 'coord':{'否':(251, 415),'是':(222,395)}},
+        {'item':'NVa3r', 'type':'str' , 'source':'reason(row[13])', 'coord':(305,395)},
+        {'item':'NVa4', 'type':'opt'  , 'source':'value(row[14])', 'coord':{'否':(205, 376),'是':(256,376),'安樂死':(205,357)}},
+        {'item':'NVa4r1', 'type':'str', 'source':'reason(row[14]) if \'是\' in row[14][0] else \' \'', 'coord':(307,376)},
+        {'item':'NVa4r2', 'type':'str', 'source':'reason(row[14]) if \'安樂死\' in row[14] else \' \'', 'coord':(285,357)},
+        {'item':'NVa5', 'type':'opt'  , 'source':'row[7]', 'coord':{'公':(216, 338),'母':(280,338)}},
+        {'item':'NVa6', 'type':'str'  , 'source':'str(row[12])', 'coord':(238, 307)},
+        {'item':'NVa7', 'type':'opt'  , 'source':'\'有\'', 'coord':{'有':(251, 280),'無':(333,280)}}, #constant option
+        {'item':'NVimg', 'type':'img' , 'source':'row[3]+\'post\'', 'coord':(421, 307), 'width':120},
+        {'item':'Ra1', 'type':'str'   , 'source':'str(row[10])', 'coord':(230, 240)},
+        {'item':'Ra2', 'type':'opt'   , 'source':'\'補助\'', 'coord':{'補助':(200, 197),'自費':(257, 197)}}, #constant option
         #'Rimg' : {'type':'img' , 'source':'stamp', 'coord':(197, 118)},
         #'Rar': {'type':'str' , 'source':'row[40]', 'coord':(424, 117)},
-    }
+    ]
     canvas.setFont('STHeiti', 12)
-    canvas.setFillColorRGB(1,0,0)
+    canvas.setFillColor(red)
 
-    for item in form.values():
+    for item in form_items:
         itype, isource, icoord = item['type'], item['source'], item['coord']
-        if itype == 'str':
-            canvas.drawString(*icoord, eval(isource))
-        if itype == 'opt':
-            canvas.drawString(*icoord[eval(isource)],'V')
-        if itype == 'img':
-            im = ImageReader("img/"+isource+".jpg")
-            canvas.drawImage(im, *icoord, width=100,height=100)
+        try:
+            if itype == 'str':
+                canvas.drawString(*icoord, eval(isource))
+            if itype == 'opt':
+                canvas.drawString(*icoord[eval(isource)],'V')
+            if itype == 'img':
+                img_exp = 'img/'+eval(isource)+'.*'
+                img_name = glob(img_exp)[0]
+                im = ImageReader(img_name)
+                imw, imh = im.getSize()
+                aspect = imh / float(imw)
+                
+                # determine the width and height
+                w, h = item.get('width', None), item.get('height', None)
+                if [w, h] == [None, None]:
+                    w, h = 100, 100
+                elif w is None:
+                    w = h * aspect
+                elif h is None:
+                    h = w * aspect
+                
+                canvas.drawImage(im, *icoord, width=w,height=h)
+        except:
+            print('[Error] 自動填寫 ' + form_name + ' 的 ' + item['item'] + ' 項目時發生錯誤')
+            
+            column_indexs = []
+            index_starts = [m.start()+4 for m in re.finditer('row\[', isource)]
+            for idx in index_starts:
+                idx_end = isource.find(']', idx)
+                column_indexs.append(int(isource[idx : idx_end]))
+            
+            print('問提出在 xlsx 資料表中第 ' + str(row_index + 1) + ' 筆有效資料的第 ' + str(column_indexs) + ' 欄' )
+            print('相關的欄位內容：')
+            for index in column_indexs:
+                print(row[index])
+                
+            canvas.setFillColor(yellow)
+            coords = list(icoord.values()) if type(icoord) is dict else icoord
 
-
-
-
-
-
-
-
+            for coord in coords:
+                canvas.rect(*coord , 10.0, 10.0, fill=1)
+            canvas.setFillColor(red)
 
 
 if __name__ == '__main__':
-    xlsx_data = loader('data/example.xlsx').data
+    #xlsx_data = loader('data/tnvr 工具輸入檔.xlsx').data
+    xlsx_data = loader('data/error_example.xlsx').data
     generate_all_forms( data_set=xlsx_data,
                         form_filler=tnvr_form_filler,
                         out_filename='output.pdf',
