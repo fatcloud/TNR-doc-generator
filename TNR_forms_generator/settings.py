5bfc0d296b0467f4db4d95f8a94326943310a57b
fatcloud <victor19871226@hotmail.com>
2017-01-15 10:33:17 +0800

    -debug

--- TNR_forms_generator/settings.py
+++ TNR_forms_generator/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = bool(os.environ.get('DJANGO_DEBUG', ''))
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', ''))
+#DEBUG = True
 
 ALLOWED_HOSTS = []
 
